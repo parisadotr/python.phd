cd954c46b6e2612d0526ecfcf3f2f54ea363d6ea
parisadotr <roshaniparisa@outlook.com>
2024-11-15 13:43:18 +0100

    Update exemple_script.py.py

--- CAMEA_dataprocess_main/example/exemple_script.py.py
+++ CAMEA_dataprocess_main/example/exemple_script.py.py
@@ -1,19 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2024, Enrico Development Team.
-# Distributed under the LGPLv2.1+ License.
-"""
-Main program test run in a Pythonic way.
 
-It decompress the data (from .zip) and run direclty the main function.
-
-"""
-import os
-import zipfile
-from pydec.core.main import pydec_main
-# pylint: disable=C0103
-
-source_zip = 'testfiles.zip'
-with zipfile.ZipFile(source_zip) as zipped:
-    zipped.extractall(path=os.path.dirname(os.path.abspath(source_zip)))
-
-pydec_main('test.json')
